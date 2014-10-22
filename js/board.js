3db3ef06126cac2fe9b2f7dfb3fef7253d6109f9
Justin Venezuela <jven@mit.edu>
2014-10-22 01:03:34 -0400

    Flips the board when playing black.

--- js/board.js
+++ js/board.js
@@ -88,9 +88,10 @@ var Board = function() {
 };
 
 
-Board.prototype.initializeWithLine = function(line) {
-  this.line_ = line;
+Board.prototype.initialize = function(moves, isWhite) {
+  this.line_ = new Line(moves, isWhite);
   this.board_.start();
+  this.board_.orientation(isWhite ? 'white' : 'black');
 };
 
 
@@ -115,23 +116,23 @@ Board.prototype.onDrop_ = function(
 
 var main = function() {
   var b = new Board();
-  var line = new Line(
-      [
-        new Move('d2', 'd4'),
-        new Move('d7', 'd5'),
-        new Move('c2', 'c4'),
-        new Move('d5', 'c4'),
-        new Move('g1', 'f3'),
-        new Move('b7', 'b5'),
-        new Move('a2', 'a4'),
-        new Move('c7', 'c6'),
-        new Move('a4', 'b5'),
-        new Move('c6', 'b5'),
-        new Move('b1', 'c3'),
-        new Move('a7', 'a6'),
-        new Move('c3', 'b5')
-      ], true /* isWhite */);
-  b.initializeWithLine(line);
+  var moves = [
+    new Move('d2', 'd4'),
+    new Move('d7', 'd5'),
+    new Move('c2', 'c4'),
+    new Move('d5', 'c4'),
+    new Move('g1', 'f3'),
+    new Move('b7', 'b5'),
+    new Move('a2', 'a4'),
+    new Move('c7', 'c6'),
+    new Move('a4', 'b5'),
+    new Move('c6', 'b5'),
+    new Move('b1', 'c3'),
+    new Move('a7', 'a6'),
+    new Move('c3', 'b5')
+  ];
+  var isWhite = false;
+  b.initialize(moves, isWhite);
 };
 
 

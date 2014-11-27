e0a2eece125b094266b5b144b1968a64bd49b800
Justin Venezuela <jven@mit.edu>
2014-11-27 03:47:34 -0500

    Added some lines for the French defense, advance variation. Added support for castling moves.

--- js/board.js
+++ js/board.js
@@ -115,9 +115,38 @@ Board.prototype.initialize = function(moves, isWhite) {
  */
 Board.prototype.onChange_ = function(oldPos, newPos) {
   if (this.line_.isComputerMoveNext()) {
-    var computerMove = this.line_.advance();
-    setTimeout(
-        this.board_.move.bind(this.board_, computerMove.asString()), 500);
+    var computerMoveString = this.line_.advance().asString();
+    var computerMoveFn;
+    if (computerMoveString == 'e1-g1') {
+      // White 0-0
+      computerMoveFn = function() {
+        this.move('e1-g1');
+        this.move('h1-f1');
+      };
+    } else if (computerMoveString == 'e1-c1') {
+      // White 0-0-0
+      computerMoveFn = function() {
+        this.move('e1-c1');
+        this.move('a1-d1');
+      };
+    } else if (computerMoveString == 'e8-g8') {
+      // Black 0-0
+      computerMoveFn = function() {
+        this.move('e8-g8');
+        this.move('h8-f8');
+      };
+    } else if (computerMoveString == 'e8-c8') {
+      // Black 0-0-0
+      computerMoveFn = function() {
+        this.move('e8-c8');
+        this.move('a8-d8');
+      };
+    } else {
+      computerMoveFn = function() {
+        this.move(computerMoveString);
+      };
+    }
+    setTimeout(computerMoveFn.bind(this.board_), 500);
   }
 
   if (this.line_.isDone()) {
@@ -148,7 +177,7 @@ Board.prototype.onDrop_ = function(
 var main = function() {
   var b = null;
   var openings = [];
-  openings = openings.concat(getOpening_benkoFullyAccepted());
+  openings = openings.concat(getOpening_frenchAdvance());
 
   var onDoneFn = function() {
     var opening = openings[Math.floor(Math.random() * openings.length)];
@@ -158,6 +187,89 @@ var main = function() {
   };
   var b = new Board(onDoneFn);
   onDoneFn();
+};
+
+var getOpening_frenchAdvance = function() {
+  var opening = 'e2 e4, e7 e6, d2 d4, d7 d5, e4 e5, c7 c5';
+  var continuations = [
+    // 4. c3
+    //'c2 c3, b8 c6, g1 f3, d8 b6, a2 a3, ',
+    'c2 c3, b8 c6, g1 f3, d8 b6, f1 d3, c5 d4, c3 d4, c8 d7, e1 g1, c6 d4, ' +
+        'f3 d4, b6 d4',
+    'c2 c3, b8 c6, g1 f3, d8 b6, f1 e2, g8 h6, b2 b3, c5 d4, c3 d4, h6 f5, ' +
+        'c1 b2, f8 b4, e1 f1, e8 g8',
+    'c2 c3, b8 c6, g1 f3, d8 b6, f1 e2, g8 h6, c1 h6, b6 b2, b1 d2, g7 h6, ' +
+        'a1 c1, b2 a2',
+    'c2 c3, b8 c6, g1 f3, d8 b6, f1 e2, g8 h6, c1 h6, b6 b2, h6 g7, f8 g7, ' +
+        'b1 d2, b2 c3, a1 c1, c3 a3, e1 g1, c5 d4',
+    'c2 c3, b8 c6, g1 f3, d8 b6, f1 e2, g8 h6, c1 h6, b6 b2, h6 g7, f8 g7, ' +
+        'b1 d2, b2 c3, a1 c1, c3 a3, d4 c5, c6 e5',
+
+    'c2 c3, b8 c6, c1 e3, d8 b6, d1 d2, c8 d7, g1 f3, a8 c8, d4 c5, f8 c5, ' +
+        'e3 c5, b6 c5',
+    'c2 c3, b8 c6, c1 e3, d8 b6, d1 d2, c8 d7, g1 f3, a8 c8, f1 e2, c5 d4, ' +
+        'c3 d4, f8 b4, b1 c3, c6 a5, e1 g1, a5 c4',
+    'c2 c3, b8 c6, c1 e3, d8 b6, d1 d2, c8 d7, g1 f3, a8 c8, f1 d3, c5 d4, ' +
+        'c3 d4, f8 b4, b1 c3, c6 a5, e1 g1, a5 c4',
+    'c2 c3, b8 c6, c1 e3, d8 b6, b2 b3, c5 d4, c3 d4, f8 b4, e3 d2, b6 d4',
+    'c2 c3, b8 c6, c1 e3, d8 b6, b2 b3, c5 d4, c3 d4, f8 b4, b1 d2, b4 c3, ' +
+        'a1 c1, c3 b2, c1 b1, b2 d4',
+    'c2 c3, b8 c6, c1 e3, d8 b6, b2 b3, c5 d4, e3 d4, c6 d4, c3 d4, f8 b4, ' +
+        'b1 d2, b4 c3, a1 c1, c3 b2, c1 b1, b2 d4',
+
+    // 4. Nf3
+    'g1 f3, c5 d4, f1 d3, b8 c6, e1 g1, g8 e7, b1 d2, e7 g6',
+    'g1 f3, c5 d4, d1 d4, b8 c6, d4 f4, g8 e7, f1 d3, e7 g6, d3 g6, h7 g6, ' +
+        'f3 g5, f7 f6, e5 f6, g7 f6, g5 f3, f8 h6',
+    'g1 f3, c5 d4, d1 d4, b8 c6, d4 g4, g8 e7, f1 d3, e7 g6, d3 g6, h7 g6',
+    'g1 f3, c5 d4, d1 d4, b8 c6, f1 b5, c8 d7, b5 c6, b7 c6',
+    'g1 f3, c5 d4, f3 d4, b8 c6, d4 c6, b7 c6',
+
+    // 4. dxc5
+    'd4 c5, f8 c5, d1 g4, e8 f8, f1 d3, d8 b6',
+    'd4 c5, f8 c5, g1 f3, b8 c6'
+  ];
+  return {
+    lines: getLinesFromOpening(opening, continuations),
+    isWhite: false
+  };
+};
+
+var getOpening_qga = function() {
+  var opening = 'd2 d4, d7 d5, c2 c4, d5 c4, g1 f3';
+  var continuations = [
+    // 3 ... Nf6
+
+    // 3 ... a6
+
+    // 3 ... c6
+
+    // 3 ... c5
+
+    // 3 ... e6
+
+    // 3 ... Bf5
+
+    // 3 ... Nc6
+
+    // 3 ... Bg4
+
+    // 3 ... b5
+    //'b7 b5, a2 a4, b5 b4, ',
+    'b7 b5, a2 a4, c7 c6, e2 e3, e7 e6, ',
+    'b7 b5, a2 a4, c7 c6, e2 e3, g8 f6, ',
+    'b7 b5, a2 a4, c7 c6, e2 e3, a7 a6, ',
+    'b7 b5, a2 a4, c8 d7, a4 b5, d7 b5, b1 c3, c7 c6, c3 b5, c6 b5, b2 b3',
+    'b7 b5, a2 a4, c8 d7, a4 b5, d7 b5, b1 c3, d8 d7, f3 e5',
+    'b7 b5, a2 a4, a7 a6, a4 b5, a6 b5, a1 a8',
+
+    // 3 ... Be6
+  ];
+
+  return {
+    lines: getLinesFromOpening(opening, continuations),
+    isWhite: true
+  };
 };
 
 var getOpening_benkoFullyAccepted = function() {
@@ -219,7 +331,7 @@ var getOpening_benkoFullyAccepted = function() {
     'b2 b3, f8 g7, c1 b2, c8 a6, g2 g3, d7 d6, f1 g2, b8 d7, g1 h3, e8 g8',
 
     // 6. e3
-    'e2 e3, d7 d6, b1 c3, f8 g7, g1 f3, e3 e4, b8 d7, f1 e2, c8 a6',
+    'e2 e3, d7 d6, b1 c3, f8 g7, g1 f3, e8 g8, e3 e4, b8 d7, f1 e2, c8 a6',
     'e2 e3, d7 d6, b1 c3, f8 g7, f1 b5, b8 d7, g1 f3, c8 a6',
     'e2 e3, d7 d6, b1 c3, f8 g7, e3 e4, e8 g8, f2 f4, d8 a5, c1 d2, c8 a6, ' +
         'f1 a6, b8 a6, g1 f3, a6 b4, e1 g1, c5 c4',
